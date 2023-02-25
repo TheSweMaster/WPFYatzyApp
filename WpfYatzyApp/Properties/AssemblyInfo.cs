187cc2262fe62f8e11e1387093602aa8078992e8
Jacob Gandemo <theswemaster@gmail.com>
2023-02-25 21:43:52 +0100

    Update to .NET 4.8 Release v1.4

--- WpfYatzyApp/Properties/AssemblyInfo.cs
+++ WpfYatzyApp/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WpfYatzyApp")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
